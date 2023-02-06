dc82df78968d0ba3c1f23cfdd5ccc9a1b74eb0c7
mazenLavey <122162390+mazenLavey@users.noreply.github.com>
2023-02-07 02:26:08 +0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ window.addEventListener('scroll', ()=>{
     let h = document.getElementById('projects').offsetTop / 2;
     if (window.scrollY >= h) {
         projectCards.forEach((card)=>{card.style = 'transform: translateX(0); opacity: 1;';})           
-        projectImgs.forEach((img)=>{img.style = 'transform: translateX(0); opacity: 1; pointer-events: none;';})
+        projectImgs.forEach((img)=>{img.style = 'transform: translateX(0); opacity: 1';})
         if (times === 0) {
             setTimeout(()=>{
                 projectImgs.forEach((img)=>{img.style = 'animation: filpCard 2s 1 alternate none; transform: translateX(0); opacity: 1';})
